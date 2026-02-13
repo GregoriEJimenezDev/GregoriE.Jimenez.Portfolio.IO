421adeb66fd030ca2ea058fd774ed5a150001072
Gregori Evangelista Jimenez <gregorievangelista43@gmail.com>
2026-02-13 08:26:32 -0400

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   base: '/GregoriE.Jimenez.Portfolio.IO',
   
   vite: {
-    plugins: [tailwindcss()],
+    Plugin: [tailwindcss()],
   },
 
   integrations: [react()],
